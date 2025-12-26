1516588f18fe4d15167f2ac73fe9b751bcc89ccc
hzz110 <hzz110@gmail.com>
2025-12-26 20:45:15 +0800

    Fix white screen issue: safely access Quill and wrap registration in try-catch

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,50 +1,57 @@
 import { useMemo, useRef, useEffect, useState } from 'react'
-import ReactQuill, { Quill } from 'react-quill'
+import ReactQuill from 'react-quill'
 import BlotFormatter from 'quill-blot-formatter'
 import 'react-quill/dist/quill.snow.css'
 import ImageModal from './ImageModal'
 import './RichTextEditor.css'
 
+// 安全地获取 Quill 对象
+const Quill = (ReactQuill as any).Quill || ReactQuill
+
 // 注册自定义 Image Blot 以支持 resizing 样式
-const BaseImage = Quill.import('formats/image')
-class ImageBlot extends BaseImage {
-  static create(value: any) {
-    const node = super.create(value)
-    if (typeof value === 'string') {
-      node.setAttribute('src', value)
-    }
-    return node
-  }
-
-  static formats(node: HTMLElement) {
-    const formats: any = {}
-    if (node.hasAttribute('width')) formats.width = node.getAttribute('width')
-    if (node.hasAttribute('height')) formats.height = node.getAttribute('height')
-    if (node.hasAttribute('style')) formats.style = node.getAttribute('style')
-    return formats
-  }
-
-  format(name: string, value: any) {
-    if (name === 'width' || name === 'height') {
-      if (value) {
-        this.domNode.setAttribute(name, value)
-      } else {
-        this.domNode.removeAttribute(name)
+// 使用 try-catch 防止注册失败导致白屏
+try {
+  const BaseImage = Quill.import('formats/image')
+  class ImageBlot extends BaseImage {
+    static create(value: any) {
+      const node = super.create(value)
+      if (typeof value === 'string') {
+        node.setAttribute('src', value)
       }
-    } else if (name === 'style') {
-      if (value) {
-        this.domNode.setAttribute(name, value)
+      return node
+    }
+
+    static formats(node: HTMLElement) {
+      const formats: any = {}
+      if (node.hasAttribute('width')) formats.width = node.getAttribute('width')
+      if (node.hasAttribute('height')) formats.height = node.getAttribute('height')
+      if (node.hasAttribute('style')) formats.style = node.getAttribute('style')
+      return formats
+    }
+
+    format(name: string, value: any) {
+      if (name === 'width' || name === 'height') {
+        if (value) {
+          this.domNode.setAttribute(name, value)
+        } else {
+          this.domNode.removeAttribute(name)
+        }
+      } else if (name === 'style') {
+        if (value) {
+          this.domNode.setAttribute(name, value)
+        } else {
+          this.domNode.removeAttribute(name)
+        }
       } else {
-        this.domNode.removeAttribute(name)
+        super.format(name, value)
       }
-    } else {
-      super.format(name, value)
     }
   }
+  Quill.register('formats/image', ImageBlot, true)
+  Quill.register('modules/blotFormatter', BlotFormatter)
+} catch (error) {
+  console.error('Error registering Quill modules:', error)
 }
-Quill.register('formats/image', ImageBlot, true)
-
-Quill.register('modules/blotFormatter', BlotFormatter)
 
 interface RichTextEditorProps {
   value: string
